687602727eaed7e3aef80f152f9e04fd652a70da
DuongTheNgoc <thngoec@gmail.com>
2025-08-15 21:59:59 +0700

    Update QRCodeSection.jsx

--- src/components/customer/payment/QRCodeSection.jsx
+++ src/components/customer/payment/QRCodeSection.jsx
@@ -5,7 +5,7 @@ function QRCodeSection() {
         <div className="mb-6">
             <p className="text-gray-700 mb-2">Quét mã QR bằng ứng dụng ngân hàng:</p>
             <img
-                src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=ThanhToan_DonHang_NguyenLieu_NauAn"
+                src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=ThanhToan_DonHang"
                 alt="QR Code"
                 className="mx-auto border rounded-md"
             />
